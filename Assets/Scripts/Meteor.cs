bebfd7f3642903d93eb0a036066658b1a49f855d
Pierre-Olivier <po.gillier@gmail.com>
2020-02-01 13:13:27 +0100

    Added Meteor Prefab

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{   
+    public float speed = 5f;
+    float CurrentTime = 0f;
+
+    public float interval = 1f;
+    // Start is called before the first frame update
+
+    Vector3 originalPosition;
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentTime += Time.deltaTime;
+        transform.localPosition = originalPosition + (new Vector3(0,0,Mathf.Sin(CurrentTime * speed))* interval);
+    }
+
+}
