e5a29ae4240380267e11916ef9d773f8e8688331
Fabien <f.cheftel@gmail.com>
2020-02-01 20:49:35 +0100

    Update ElementsColorCheck.cs

--- Assets/Scripts/ElementsColorCheck.cs
+++ Assets/Scripts/ElementsColorCheck.cs
@@ -38,7 +38,6 @@ public class ElementsColorCheck : MonoBehaviour
 
     public void CheckElementsColorMatch(string elementName, Material paintingToolMaterial, MeshRenderer elementMeshRendererRef)
     {
-        print("101");
         if (m_elementsMaterials != null && m_elementsMaterials.Count != 0)
         {
             for (int i = 0; i < m_elementsMaterials.Count; i++)
@@ -50,26 +49,21 @@ public class ElementsColorCheck : MonoBehaviour
 
     private IEnumerator CheckElementsColorMatchCoroutine(string elementName, Material paintingToolMaterial, MeshRenderer elementMeshRendererRef)
     {
-        print("102");
         //Switch over all possible color code we defined as elements expected color
         switch (paintingToolMaterial.name)
         {
             case "Grey":
-                print("103: " + elementName + " - " + elementName.Contains("Building"));
                 if (elementName.Contains("Building") || elementName.Contains("Mountains"))
                 {
-                    print("104");
                     Material tmpElementMaterial = FindElementDefaultMaterial(elementName);
                     //Get the default matching material based on designed element name
                     if (tmpElementMaterial != null)
                     {
-                        print("105");
                         elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
                     }
                 }
                 else
                 {
-                    print("107");
                     elementMeshRendererRef.sharedMaterial = paintingToolMaterial;
                 }
                 break;
@@ -93,10 +87,8 @@ public class ElementsColorCheck : MonoBehaviour
     {
         foreach (var elementMaterial in m_elementsMaterials)
         {
-            print("105.5: " + elementMaterial.Key + " - " + elementName);
             if (elementName.Contains(elementMaterial.Key))
             {
-                print("106.5");
                 return elementMaterial.Value;
             }
         }
