c4b98ab3feb963992de6e4e45f9d18e16176d20c
Rivemale Loïck <lo.rivemale@gmail.com>
2020-02-02 15:18:58 +0100

    Added working biomes.

--- Assets/Scripts/ElementsColorCheck.cs
+++ Assets/Scripts/ElementsColorCheck.cs
@@ -31,7 +31,18 @@ public class ElementsColorCheck : MonoBehaviour
         {
             for (int i = 0; i < elements.Count; i++)
             {
-                m_elementsMaterials.Add(elements[i].name, elements[i].GetComponentInChildren<MeshRenderer>().sharedMaterial);
+                var rend = elements[i].GetComponentInChildren<MeshRenderer>();
+
+                if (rend)
+                {
+                    m_elementsMaterials.Add(
+                        elements[i].name,
+                        rend.sharedMaterial);
+                }
+                else
+                {
+                    print(elements[i].name);
+                }
             }
         }
     }
@@ -48,14 +59,27 @@ public class ElementsColorCheck : MonoBehaviour
         }
     }
 
-    private IEnumerator CheckElementsColorMatchCoroutine(string elementName, Material paintingToolMaterial, MeshRenderer elementMeshRendererRef)
+    #region my duplicate
+
+    public void smr_CheckElementsColorMatch(string elementName, Material paintingToolMaterial, SkinnedMeshRenderer elementMeshRendererRef)
+    {
+        if (m_elementsMaterials != null && m_elementsMaterials.Count != 0)
+        {
+            //For each element's material in our base list, we will check if it's a color match with the current brush material
+            for (int i = 0; i < m_elementsMaterials.Count; i++)
+            {
+                StartCoroutine(smr_CheckElementsColorMatchCoroutine(elementName, paintingToolMaterial, elementMeshRendererRef));
+            }
+        }
+    }
+
+    private IEnumerator smr_CheckElementsColorMatchCoroutine(string elementName, Material paintingToolMaterial, SkinnedMeshRenderer elementMeshRendererRef)
     {
         Material tmpElementMaterial;
         //Check over all possible color code we defined as element's expected color
         //If it's a match we set the designed element to rightful default matching material
-        if (paintingToolMaterial.name == "Grey" && (elementName.Contains("Building") || elementName.Contains("Mountain")))
+        if (paintingToolMaterial.name == "Grey" && (elementName.Contains("Building") || elementName.Contains("Mountains")))
         {
-            print("Building !");
             tmpElementMaterial = FindElementDefaultMaterial(elementName);
             if (tmpElementMaterial != null)
                 elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
@@ -107,7 +131,94 @@ public class ElementsColorCheck : MonoBehaviour
         yield return null;
     }
 
-    private Material FindElementDefaultMaterial(string elementName)
+    #endregion
+
+    private IEnumerator CheckElementsColorMatchCoroutine(string elementName, Material paintingToolMaterial, MeshRenderer elementMeshRendererRef)
+    {
+        Material tmpElementMaterial;
+        //Check over all possible color code we defined as element's expected color
+        //If it's a match we set the designed element to rightful default matching material
+        if (paintingToolMaterial.name == "Grey" && (elementName.Contains("Building") || elementName.Contains("Mountains")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else if (paintingToolMaterial.name == "Green" &&
+                (elementName.Contains("Tree") || elementName.Contains("Bird") || elementName.Contains("Hill")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else if (paintingToolMaterial.name == "Brown" && (elementName.Contains("Horse")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else if (paintingToolMaterial.name == "Red" &&
+                (elementName.Contains("Bird") || elementName.Contains("Flower")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else if (paintingToolMaterial.name == "Purple" && (elementName.Contains("Flower")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else if (paintingToolMaterial.name == "Yellow" &&
+                (elementName.Contains("Flower") || elementName.Contains("Desert")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else if (paintingToolMaterial.name == "Blue" &&
+                (elementName.Contains("Flower") || elementName.Contains("Ocean") || elementName.Contains("Snow")))
+        {
+            tmpElementMaterial = FindElementDefaultMaterial(elementName);
+            ApplyColor(elementMeshRendererRef, tmpElementMaterial);
+            //if (tmpElementMaterial != null)
+            //    elementMeshRendererRef.sharedMaterial = tmpElementMaterial;
+        }
+        else //Brush's color and element's color are not matching, so we set it to the current color on the brush
+        {
+            elementMeshRendererRef.sharedMaterial = paintingToolMaterial;
+        }
+
+        yield return null;
+    }
+
+    void ApplyColor(MeshRenderer rend, Material correctMaterial)
+    {
+        if (correctMaterial != null)
+        {
+            rend.sharedMaterial = correctMaterial;
+
+            var colorcomp = rend.gameObject.GetComponent<ColorComponent>();
+
+            if (colorcomp != null)
+            {
+                colorcomp.ForceFinish();
+            }
+            else
+            {
+                print("Color comp == null");
+            }
+        }
+    }
+
+    public Material FindElementDefaultMaterial(string elementName)
     {
         foreach (var elementMaterial in m_elementsMaterials)
         {
