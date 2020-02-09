3bdc78cbdd1a4a32395456f766c957a2116ab4de
Rivemale Loïck <lo.rivemale@gmail.com>
2020-02-09 13:20:42 +0100

    Added the brush behavior + sound and effect. Added a prefab for vr test. Tried a few draw on top particles shader.

---
+++ Assets/Scripts/Brush/BrushBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushBehavior : MonoBehaviour
+{
+    public AudioSource source;
+    public ParticleSystem particles;
+    public Material BurstMat;
+
+    //private void Update()
+    //{
+    //    particles.Play();
+    //}
+    private void Start()
+    {
+        Application.targetFrameRate = -1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ColorPicker"))
+        {
+            PlayFeedback(other);
+        }
+    }
+
+    void PlayFeedback(Collider other)
+    {
+        BurstMat.SetColor("_TintColor", other.GetComponent<MeshRenderer>().sharedMaterial.color);
+        particles.Play();
+    }
+}
